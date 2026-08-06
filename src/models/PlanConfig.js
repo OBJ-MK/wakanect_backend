d91b39750305657e9dc212065c07d1d4126f84c4
Modibo Kane <kaneferme1@gmail.com>
2026-08-06 00:42:52 +0000

    fix: import manquant getCountryTier dans PlanConfig.computePrice

--- src/models/PlanConfig.js
+++ src/models/PlanConfig.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const mongoose = require('mongoose');
+const { getCountryTier } = require('../constants/countries');
 
 /**
  * Singleton config des plans — une seule doc en base, key='main'.
